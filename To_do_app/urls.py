5a84a2359f4855a12c0781538596db68a4a61549
anujdev11 <adev23452@gmail.com>
2020-01-06 16:27:44 +0530

    Basic

---
+++ To_do_app/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('add_todo/',views.add_todo),
+    path('delete_todo/<int:todo_id>/', views.delete_todo),
+]
